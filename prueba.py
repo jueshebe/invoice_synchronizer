6e621cabd52944954876aa73a8f3ab452e5bd230
JulianEstebanHerrera <julianesteban.herrera@mercadolibre.com.co>
2022-09-25 00:15:55 -0500

    adding mypy fix

--- prueba.py
+++ prueba.py
@@ -1,15 +1,9 @@
-from sqlite3 import connect
 from pirpos2siigo import Connector
-from pirpos2siigo import utils
-import numpy as np
-import pandas as pd
 
 # instanciar objeto
 connector = Connector(configuration_path="configuration.JSON")
-
-
 connector.update_invoices("2022-08-01", "2022-08-31")
+
 pass
 
 # hacer que haga intentos y retome los errores del json
-
